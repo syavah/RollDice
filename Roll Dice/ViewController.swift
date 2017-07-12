4584090946e85b0c4a809e277b8152366e62373d
syavah <syavah@gmail.com>
2017-07-13 00:25:49 +0300

    1

--- Roll Dice/ViewController.swift
+++ Roll Dice/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        rollDice()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func rollACTION(_ sender: UIButton) {
+        
+        rollDice()
+    }
+    
+    func rollDice(){
+        
+        let numberOne = arc4random_uniform(6) + 1
+        
+        let numberTwo = arc4random_uniform(6) + 1
+        
+        label.text = "The sum is: \(numberOne + numberTwo)"
+        
+        leftImageView.image = UIImage(named: "Dice\(numberOne)")
+        
+        rightImageView.image = UIImage(named: "Dice\(numberTwo)")
+    }
 
 }
 
